2aaa8ab048cb3625b88a473f49ffd8565c639c2a
Trevahr Robertson <trevahr@yahoo.com>
2019-03-13 21:04:22 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -3,7 +3,7 @@ var a = {}
 function forLoop(a){
   
   for (var i = 0; i < 25; i++){
-    if (i = 1) {
+    if (i === 1) {
       console.log("I am 1 strange loop.")
     }
     else `I am ${i} strange loops.`
