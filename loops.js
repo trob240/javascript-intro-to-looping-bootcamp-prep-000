5d87cdece52676a947ae7e7a8d9c902b02f58794
Trevahr Robertson <trevahr@yahoo.com>
2019-03-13 21:07:55 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -6,7 +6,7 @@ function forLoop(a){
     if (i === 1) {
       a.push("I am 1 strange loop.")
     }
-    else {`I am ${i} strange loops.`}
+    else {a.push(`I am ${i} strange loops.`)}
   }
   
 }
