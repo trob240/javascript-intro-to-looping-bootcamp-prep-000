6ba0c4f7a04b231bd2011dde3941496cd6a3daa8
Trevahr Robertson <trevahr@yahoo.com>
2019-03-13 21:48:32 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -21,7 +21,7 @@ function whileLoop(number){
 
 function doWhileLoop(a){
   do {
-    conosole.log('I run once regardless.');
+    console.log('I run once regardless.');
   }
   while(incrementVariable(a) < num)
 }
